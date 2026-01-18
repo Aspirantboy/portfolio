a375eb6383293db5a329d7a0e0d417709dec68c4
Aspirantboy <b22me025@nitm.ac.in>
2026-01-18 12:13:58 +0530

    first commit

--- src/content_option.js
+++ src/content_option.js
@@ -12,7 +12,7 @@ const introdata = {
         third: "I am a Student",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    your_img_url: "https://res.cloudinary.com/dwqusdhzz/image/upload/v1768718193/IMG-20241228-WA0038_fipdzn.jpg",
 };
 
 const dataabout = {
